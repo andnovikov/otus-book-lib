9fc3363959988e4356dd5c301ec0ead01e585252
anovikov <andnovikov@outlook.com>
2019-11-09 12:58:38 +0300

    Spring initializr.

---
+++ src/main/java/ru/anovikov/learning/otusbooklib/OtusBookLibApplication.java
@@ -0,0 +1,13 @@
+package ru.anovikov.learning.otusbooklib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusBookLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusBookLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/anovikov/learning/otusbooklib/OtusBookLibApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.anovikov.learning.otusbooklib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusBookLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
