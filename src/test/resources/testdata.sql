ad38c6fcd5936f6cbb0963c2f1bd991d2d6cff86
anovikov <andnovikov@outlook.com>
2019-11-11 00:06:37 +0300

    Properties and schema.

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -1,2 +1,11 @@
 insert into authors (id, firstName, lastName) values (1, 'Федор', 'Достоевский');
 insert into authors (id, firstName, lastName) values (2, 'Лев', 'Толстой');
+insert into authors (id, firstName, lastName) values (3, 'TestDelFirstname', 'TestDelLastname');
+insert into authors (id, firstName, lastName) values (4, 'TestUpdFirstname', 'TestUpdLastname');
+
+insert into genres(id, genreName) values (1, 'Роман');
+insert into genres(id, genreName) values (2, 'Повесть');
+insert into genres(id, genreName) values (3, 'TestDel');
+insert into genres(id, genreName) values (4, 'TestUpd');
+
+insert into books(id, authorId, genreId, title) values (1, 1, 1, "Преступление и наказание");
