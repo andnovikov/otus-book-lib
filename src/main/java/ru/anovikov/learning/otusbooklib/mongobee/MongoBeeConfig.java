387b0c5860a161f7f3df64e9e3e11d28bf945003
anovikov <andnovikov@outlook.com>
2020-01-09 23:47:50 +0300

    Heroku configs

--- src/main/java/ru/anovikov/learning/otusbooklib/mongobee/MongoBeeConfig.java
+++ src/main/java/ru/anovikov/learning/otusbooklib/mongobee/MongoBeeConfig.java
@@ -3,6 +3,7 @@ package ru.anovikov.learning.otusbooklib.mongobee;
 import com.github.mongobee.Mongobee;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,9 @@ import ru.anovikov.learning.otusbooklib.mongobee.changelog.DatabaseChangelog;
 @Configuration
 public class MongoBeeConfig {
 
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
     @Autowired
     private MongoClient mongo;
 
@@ -22,7 +26,7 @@ public class MongoBeeConfig {
     @Bean
     public Mongobee mongobee(Environment environment, ApplicationContext applicationContext) {
         Mongobee runner = new Mongobee(mongo);
-        runner.setDbName("otusbook");
+        runner.setDbName(database);
         runner.setChangeLogsScanPackage(DatabaseChangelog.class.getPackage().getName());
         runner.setSpringEnvironment(environment);
         runner.setMongoTemplate(mongoTemplate);
